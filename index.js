faadd163e1a2008b003e480bfd663de2a4935ab7
sumandey7419 <sumandey74192.gmail.com>
2022-10-07 16:24:13 +0530

    msg

---
+++ index.js
@@ -0,0 +1,43 @@
+let a=40;
+let b=20;
+let add=a+b;
+let sub=a-b;
+let mul=a*b;
+let div=a/b;
+let mod=a%b;
+console.log(add, sub, mul, div, mod)
+
+let one = 1;
+let four = 4;
+console.log(one < four)
+console.log(four < one)
+
+let x=3;
+let z=10;
+console.log(x<z && z>x)
+console.log(x>z || x<z)
+
+let c=10;
+let d=5;
+c += 2;
+d *= 3;
+console.log(c)
+console.log(d)
+
+let j=2;
+console.log(++j)
+let k=16;
+console.log(--k)
+
+const age=22;
+const voter=age>=18 ?
+"vote":"no vote";
+console.log(voter);
+
+let az=4;
+let bz=5;
+console.log(az | bz)
+console.log(az & bz)
+
+let siz=154;
+console.log(typeof(siz))
